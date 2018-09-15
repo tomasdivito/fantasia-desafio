84310b48f3a83b127328c510638c3174729a4c77
Tomas Di Vito <tomasdv2@gmail.com>
2018-09-15 12:42:35 -0300

    implemented base abstract class Unidad

--- src/Unidades/Unidad.java
+++ src/Unidades/Unidad.java
@@ -1,12 +1,24 @@
 package Unidades;
 
 public abstract class Unidad {
-	private double saludInicial;
+	private double salud;
 	private double ataqueBase;
 	
 	public Unidad(double saludInicial, double ataqueBase) {
-		this.saludInicial = saludInicial;
+		this.salud = saludInicial;
 		this.ataqueBase = ataqueBase;
+	}
+	
+	public double getSalud() {
+		return salud;
+	}
+	
+	public void setSalud(double salud) {
+		this.salud = salud;
+	}
+	
+	public double getAtaque() {
+		return ataqueBase;
 	}
 	
 	public abstract void atacar(Unidad unidad);
