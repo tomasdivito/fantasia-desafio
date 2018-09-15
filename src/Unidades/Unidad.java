bea354b8b4cb1bd3f113c6ac53706cf2707400b7
Tomas Di Vito <tomasdv2@gmail.com>
2018-09-15 12:48:03 -0300

    added x position in the UNIDAD

--- src/Unidades/Unidad.java
+++ src/Unidades/Unidad.java
@@ -1,12 +1,24 @@
 package Unidades;
 
 public abstract class Unidad {
+	private double x;
 	private double salud;
 	private double ataqueBase;
 	
 	public Unidad(double saludInicial, double ataqueBase) {
 		this.salud = saludInicial;
 		this.ataqueBase = ataqueBase;
+		this.x = 0;
+	}
+	
+	public Unidad(double saludInicial, double ataqueBase, double x) {
+		this.salud = saludInicial;
+		this.ataqueBase = ataqueBase;
+		this.x = x;
+	}
+	
+	public double getUbicacion() {
+		return x;
 	}
 	
 	public double getSalud() {
