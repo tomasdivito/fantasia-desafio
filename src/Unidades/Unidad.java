aeba3f58244310a1506b945f4777f5b2eaf029c6
Tomas Di Vito <tomasdv2@gmail.com>
2018-09-15 12:51:55 -0300

    added rango to the base class

--- src/Unidades/Unidad.java
+++ src/Unidades/Unidad.java
@@ -4,17 +4,24 @@ public abstract class Unidad {
 	private double x;
 	private double salud;
 	private double ataqueBase;
+	private double[] rango = new double[2];
 	
-	public Unidad(double saludInicial, double ataqueBase) {
+	public Unidad(double saludInicial, double ataqueBase, double[] rango) {
 		this.salud = saludInicial;
 		this.ataqueBase = ataqueBase;
 		this.x = 0;
+		this.rango = rango;
 	}
 	
-	public Unidad(double saludInicial, double ataqueBase, double x) {
+	public Unidad(double saludInicial, double ataqueBase, double[] rango, double x) {
 		this.salud = saludInicial;
 		this.ataqueBase = ataqueBase;
 		this.x = x;
+		this.rango = rango;
+	}
+	
+	public double[] getRango() {
+		return rango;
 	}
 	
 	public double getUbicacion() {
